58f3349dfe26fc0e2d2528f00aec673a2704df89
phanthaianths1975 <96382790+phanthaianths1975@users.noreply.github.com>
2022-01-19 00:16:28 +0900

    meetingroom
    meetingroom

---
+++ Emergency/Areas/MeetingRoom/Controllers/SchedulerController.cs
@@ -0,0 +1,29 @@
+﻿using Emergency.Areas.MeetingRoom.Models.SampleData;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emergency.Areas.MeetingRoom.Controllers
+{
+    [Area("MeetingRoom")]//Declare Area
+    public class SchedulerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MeetingRoom()
+        {
+            //this.schedulerControl1.Views.DayView.VisibleTime = new DevExpress.XtraScheduler.TimeOfDayInterval(System.TimeSpan.Parse("07:00:00"), System.TimeSpan.Parse("22.00:00:00"));
+            return View(SampleData.Appointments);
+        }
+        public Boolean AllowAdd()
+        {
+            if (12 > 10)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Emergency/Areas/MeetingRoom/Models/MeetingRoom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emergency.Areas.MeetingRoom.Models
+{
+    public class MeetingRoom
+    {
+        public Boolean add { get; set; }
+        public static Array RoomList()
+        {
+            return new[] {
+                new { text = "Meeting Room 1", id = 1, color = "#fcb65e" },
+                new { text = "Meeting Room 2", id = 2, color = "#e18e92" },
+                new { text = "Meeting Room 3", id = 3, color = "#e18e92" }
+            };
+        }
+        public static Boolean AllowAdd()
+        {
+            if (2 > 10)
+                return true;
+            return false;
+        }
+        public static Boolean AllowDelete()
+        {
+            return true;
+        }
+        public static Boolean AllowUpdate()
+        {
+            return true;
+        }
+    }
+}
